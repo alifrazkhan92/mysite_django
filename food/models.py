bdf89637b9fa9e426341bd9cd62fcb0ac198efe3
Ali Fraz <alifrazkhan92@gmail.com>
2024-01-17 17:31:18 +0000

    Day two

--- food/models.py
+++ food/models.py
@@ -10,5 +10,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=500)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default='https://static.wixstatic.com/media/bf242e_6133b4ae6a104cc2b50d70179f35efea~mv2.jpg/v1/fill/w_350,h_263,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/food-placeholder.jpg')
 
 
